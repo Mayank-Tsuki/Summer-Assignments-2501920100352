358f3e9a473682b3ee269eec41f37f501352b64c
Mayank-god <mrawat24680@gmail.com>
2026-06-20 21:02:58 +0530

    Create RowWiseSum.cpp

---
+++ Day20/RowWiseSum.cpp
@@ -0,0 +1,38 @@
+//Write a program to Find row-wise sum. 
+#include<bits.//stdc++.h>
+using namespace std;
+
+int main() {
+    int n , m;
+    cout<<"Enter the no of rows :";cin>>n;
+    cout<<"Enter the no of columns :";cin>>m;
+    int arr[n][m];
+
+    for ( int i = 0 ; i < n ; i++){
+        for ( int j = 0 ; j < m ; j++){
+            cout<<"Enter the value of ["<<i<<","<<j<<"] : ";cin>>arr[i][j];
+        }
+        cout<<endl;
+    }
+    cout<<"Given Matrix:"<<endl;
+    for ( int i = 0 ; i < n ; i++){
+        for ( int j = 0 ; j < m ; j++){
+            cout << arr[i][j] << " ";
+        }
+        cout<<endl;
+    }
+    cout<<endl;
+
+    //Row wise Sum 
+    for (int i = 0; i < n; i++) {
+        int sum = 0;
+        for (int j = 0; j < m; j++) {
+            sum += arr[i][j];
+        }
+        cout<<"Sum of row "<<i+1<<" is "<<sum;
+        cout << endl;
+    }
+    cout<<endl;
+    
+    return 0;
+}
