b9bb70238d267fea383f514e8feafed4156b8e31
Mayank-god <mrawat24680@gmail.com>
2026-06-25 11:12:50 +0530

    Create CommonChar.cpp

---
+++ Day25/CommonChar.cpp
@@ -0,0 +1,26 @@
+//Write a program to Find common characters in strings
+
+#include<bits.//stdc++.h>
+using namespace std;
+
+int main() {
+    string str1, str2;
+
+    cout << "Enter first string: ";
+    getline(cin, str1);
+    cout << "Enter second string: ";
+    getline(cin, str2);
+    bool flag = false;
+
+    cout<<"Common Characters are : ";
+    for (int i = 0; i<str1.length(); i++){
+        for (int j = 0; j < str2.length(); j++) {
+            if (str1[i] == str2[j]) {
+                cout<<str2[j]<<" ";
+                flag = true;
+            }
+        }
+    }
+    if(!flag) cout<<"sry, there are no common character!";
+    return 0;
+}
