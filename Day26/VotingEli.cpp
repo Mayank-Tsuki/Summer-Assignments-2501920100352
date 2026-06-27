a47005138450a813d1c2ca27a910f50084a17529
Mayank-god <mrawat24680@gmail.com>
2026-06-27 10:20:28 +0530

    Create VotingEli.cpp

---
+++ Day26/VotingEli.cpp
@@ -0,0 +1,15 @@
+//Write a program to Create voting eligibility system. 
+
+#include <bits.//stdc++.h>
+using namespace std;
+
+int main(){
+    int age;
+    cout<<"Enter Your age :-";
+    cin>>age;
+
+    if(age>=18) cout<<"Yes! you can vote.";
+    else cout<<"You can't vote till you are 18!";
+
+    return 0;
+}
